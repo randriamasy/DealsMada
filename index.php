888860f0a5cf6e17b4758ff69257595a50973a72
RANDRIAMASY <malala.randriamasygmail.com>
2016-04-10 10:41:38 +0100

    change css header

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 <body>
 <div id="header">
 <div class="container">
-<div id="welcomeLine" class="row">
+<!-- <div id="welcomeLine" class="row">
 	<div class="span6">Welcome!<strong> User</strong></div>
 	<div class="span6">
 	<div class="pull-right">
@@ -47,7 +47,7 @@
 		<a href="product_summary.html"><span class="btn btn-mini btn-primary"><i class="icon-shopping-cart icon-white"></i> [ 3 ] Itemes in your cart </span> </a> 
 	</div>
 	</div>
-</div>
+</div> -->
 <!-- Navbar ================================================== -->
 <div id="logoArea" class="navbar">
 <a id="smallScreen" data-target="#topMenu" data-toggle="collapse" class="btn btn-navbar">
